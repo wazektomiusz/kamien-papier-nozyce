20228e37ed4eaa9560a70a5f0e8aefa2a2cf53b9
wazektomiusz <106705268+wazektomiusz@users.noreply.github.com>
2022-06-01 22:24:51 +0200

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+#include <QDialog>
+#include <QObject>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->exitButton, SIGNAL(clicked()), this, SLOT(close()));
+    connect(ui->playButton, SIGNAL(clicked()), this, SLOT(playButtonClicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+/* Load the gameplay dialog
+ * Hide the main window
+ */
+void MainWindow::playButtonClicked()
+{
+    new GamePlay();
+}
